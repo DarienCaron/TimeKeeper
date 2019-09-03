813e1449b1290292dbaa8593a9704f78ddad3692
Darien Caron <caro0103@algonquinlive.com>
2019-09-03 14:27:19 -0400

    Added in simple movement
    Created a player controller script that allows the player to move on the X and Z.

---
+++ TimeKeeper-Portfolio/Assets/Scripts/Player Character/Movement/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float RotationSpeed = 3f;
+    public float MovementSpeed = 5f;
+
+
+
+    // TODO Implement basic player controllers such as rotating and moving. To be cleaned and optimized later
+
+    void Start()
+    {
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 directionInput = new Vector3(x, 0, z);
+        directionInput.Normalize();
+
+
+        transform.position += directionInput * Time.deltaTime * MovementSpeed;
+        currentDirection = directionInput;
+
+
+
+
+    }
+
+    private Vector3 currentDirection;
+    private Vector3 currentVelocity;
+}
