be9887391df5c9397471dadf6c65801cd29b5f28
Darien Caron <caro0103@algonquinlive.com>
2019-09-04 15:38:17 -0400

    Updated the character controller
    Updated the character controller with a new velocity calculation. More complex than the old system. Now the character is terrain ready.

---
+++ TimeKeeper-Portfolio/Assets/Scripts/Math/MathUtilities.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathUtilities 
+{
+    public static float CompareEpsilon = 0.00001f;
+
+    private static float HALFVALUE = 0.5f;
+    // This function will project a point inside a capsile to the bottom of the capsule.
+    // The capsule is assumed to be oriented along the y-axis.
+    public static Vector3 ProjectToBottomOfCapsule(Vector3 pointToProject, Vector3 capsuleCenter, float capsuleHeight, float capsuleRadius )
+    {
+        // Calculating the length of the line segment part of the capsule.
+       
+        float lineSegmentLength = capsuleHeight - 2.0f * capsuleRadius;
+
+        //Clamp line segment length
+        lineSegmentLength = Mathf.Max(lineSegmentLength, 0.0f);
+
+        // Calculate the line segment that goes along the capsules "Height"
+        Vector3 bottomLineSegPt = capsuleCenter;
+        bottomLineSegPt.y -= lineSegmentLength * HALFVALUE;
+
+        // Get displacement from bottom of line segment
+        Vector3 ptDisplacement = pointToProject - bottomLineSegPt;
+
+        // Calculate needed distances
+        float horizDistSqrd = ptDisplacement.x * ptDisplacement.x + ptDisplacement.z * ptDisplacement.z;
+
+
+        // Radius squared
+        float radiusSqrd = capsuleRadius * capsuleRadius;
+
+
+        // The answer will be undefined if the pt is horizontally outside of the capsule
+        if(horizDistSqrd > radiusSqrd)
+        {
+            return pointToProject;
+        }
+
+        // Calculate the projected point
+        float heightFromSegmentPt = -Mathf.Sqrt(radiusSqrd - horizDistSqrd);
+
+        Vector3 projectedPoint = pointToProject;
+        projectedPoint.y = bottomLineSegPt.y + heightFromSegmentPt;
+
+        return projectedPoint;
+
+
+
+
+    }
+
+    public static float CalculateVerticalAngle(Vector3 direction)
+    {
+        //                 /|
+        //                / |
+        //               /  |
+        //            h /   |
+        //             /    | o
+        //            /     |
+        //           /ang   |
+        //          /_______|
+        //
+        //sin(ang) = o/h, but since the dir is a unit vector h = 1
+        //The angle will be: Asin(o)
+
+        return Mathf.Rad2Deg * Mathf.Asin(direction.y);
+    }
+
+
+    //Eases from the start to the end.  This is meant to be called over many frames.  The
+    //values will change fast at first and gradually slow down.
+    public static Vector3 LerpTo(float easeSpeed, Vector3 start, Vector3 end, float dt)
+    {
+        Vector3 diff = end - start;
+
+        diff *= Mathf.Clamp(dt * easeSpeed, 0.0f, 1.0f);
+
+        return diff + start;
+    }
+
+
+
+    //Eases from the start to the end.  This is meant to be called over many frames.  The
+    //values will change fast at first and gradually slow down.
+    public static float LerpTo(float easeSpeed, float start, float end, float dt)
+    {
+        float diff = end - start;
+
+        diff *= Mathf.Clamp(dt * easeSpeed, 0.0f, 1.0f);
+
+        return diff + start;
+    }
+}
--- TimeKeeper-Portfolio/Assets/Scripts/Player Character/Movement/PlayerController.cs
+++ TimeKeeper-Portfolio/Assets/Scripts/Player Character/Movement/PlayerController.cs
@@ -2,44 +2,243 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// Cross referencing character controller made by Konstantine Titov
+
 public class PlayerController : MonoBehaviour
 {
 
-    public float RotationSpeed = 3f;
-    public float MovementSpeed = 5f;
+    [Header("Ground Variables")]
+
+    public float MovementAccelerationSpeed = 8.0f;
+    public float MaxSpeed = 10.0f;
+    public float DecelerationSpeed = 10.0f;
+
+    [Space(45)]
+
+    [Header("Rotation Variables")]
+
+    public float YawRotation = 55.0f;
 
 
+    [Space(25)]
 
-    // TODO Implement basic player controllers such as rotating and moving. To be cleaned and optimized later
+    [Header("Ground Check Values")]
+
+    public float GroundCheckStartOffsetY = 0.5f;
+    public float CheckForGroundRadius = 0.5f;
+    public float GroundResolutionOverlap = 0.05f;
+
+    public float MinAllowedSurfaceAngle = 15.0f;
+
+    public GameObject FootLocation;
+
+    [Space(35)]
+    [Header("Constants")]
+
+    public const float GroundHitAdjustment = 0.1f;
+
+  
 
     void Start()
     {
-        
+
+        m_GroundCheckMask = ~LayerMask.GetMask("Player", "Ignore Raycast");
+
+        m_RigidBody = GetComponent<Rigidbody>();
+
+        m_Velocity = Vector3.zero;
+
+
+
     }
 
-    
-    void FixedUpdate()
+    void Update()
     {
         
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        Vector3 directionInput = new Vector3(x, 0, z);
-        directionInput.Normalize();
+    }
 
-        Vector3 rot = new Vector3(0,Input.GetAxis("Mouse X"), 0);// Rotation code for now. 
+    void FixedUpdate()
+    {
+        m_Velocity = m_RigidBody.velocity;
+
+        UpdateGroundInfo();
+
+        Vector3 localMoveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        localMoveDir.Normalize();
+
+        UpdateOnGround(localMoveDir);
+
+
+      Vector3 rot =   new Vector3(
+            Input.GetAxis("Mouse Y"),
+            Input.GetAxis("Mouse X"),
+            0.0f
+            );
+
         rot.x = 0;
-        
-        transform.Rotate(rot * Time.deltaTime * RotationSpeed);//
 
-
-        transform.position += directionInput * Time.deltaTime * MovementSpeed; // Need to account for current direction.
-        currentDirection = directionInput;
-
-      
+        transform.Rotate(rot * Time.deltaTime * YawRotation);
 
 
     }
 
-    private Vector3 currentDirection;
-    private Vector3 currentVelocity;
+    void UpdateGroundInfo()
+    {
+        GroundAngularVelocity = Vector3.zero;
+        GroundVelocity = Vector3.zero;
+        GroundNormal = Vector3.forward;
+
+        m_CenterHeight = transform.position.y;
+
+        float footHeight = FootLocation.transform.position.y;
+        float halfCapsuleHeight = m_CenterHeight - footHeight;
+
+        Vector3 rayStart = transform.position;
+        rayStart.y += GroundCheckStartOffsetY;
+
+        Vector3 rayDir = Vector3.down;
+
+        float rayDist = halfCapsuleHeight + GroundCheckStartOffsetY - CheckForGroundRadius;
+
+        RaycastHit[] hitInfos = Physics.SphereCastAll(rayStart, CheckForGroundRadius, rayDir, rayDist + GroundHitAdjustment, m_GroundCheckMask);
+
+        RaycastHit groundHitInfo = new RaycastHit();
+
+        bool validGroundFound = false;
+
+        float minGroundDist = float.MaxValue;
+
+        foreach(RaycastHit hits in hitInfos)
+        {
+
+            // Replace the math equation below with a helper function. This is to calculate the vertical angle.
+            float surfaceAngle = MathUtilities.CalculateVerticalAngle(hits.normal);
+
+            if(surfaceAngle < MinAllowedSurfaceAngle || hits.distance <= 0.0f)
+            {
+                continue;
+            }
+            if(hits.distance < minGroundDist)
+            {
+                minGroundDist = hits.distance;
+                groundHitInfo = hits;
+                validGroundFound = true;
+            }
+
+        }
+
+        if(!validGroundFound)
+        {
+            // change this for when the player is in the air.
+            return;
+        }
+
+        Vector3 bottomAtHitPoint = MathUtilities.ProjectToBottomOfCapsule(groundHitInfo.point, transform.position, halfCapsuleHeight * 2.0f, CheckForGroundRadius);
+
+        float stepUpAmount = groundHitInfo.point.y - bottomAtHitPoint.y;
+        m_CenterHeight += stepUpAmount - GroundResolutionOverlap;
+
+        GroundNormal = groundHitInfo.normal;
+        
+    }
+
+    void UpdateOnGround(Vector3 localMoveDir)
+    {
+        if(localMoveDir.sqrMagnitude > MathUtilities.CompareEpsilon)
+        {
+            Vector3 localVelocity = m_Velocity - GroundVelocity;
+
+            Vector3 moveAccel = CalculateMoveAccel(localMoveDir);
+            Vector3 groundTangent = moveAccel - Vector3.Project(moveAccel, GroundNormal);
+
+            groundTangent.Normalize();
+
+            moveAccel = groundTangent;
+
+            Vector3 velocityAlongMoveDir = Vector3.Project(localVelocity, moveAccel);
+
+
+            if(Vector3.Dot(velocityAlongMoveDir, moveAccel) > 0.0f)
+            {
+                localVelocity = MathUtilities.LerpTo(DecelerationSpeed, localVelocity, velocityAlongMoveDir, Time.fixedDeltaTime);
+
+            }
+            else
+            {
+                localVelocity = MathUtilities.LerpTo(DecelerationSpeed, localVelocity, Vector3.zero, Time.fixedDeltaTime);
+            }
+
+            moveAccel *= MovementAccelerationSpeed;
+            localVelocity += moveAccel * Time.fixedDeltaTime;
+            localVelocity = Vector3.ClampMagnitude(localVelocity, MaxSpeed);
+
+            m_Velocity = localVelocity + GroundVelocity;
+
+        }
+        else
+        {
+            UpdateStopping(DecelerationSpeed);
+        }
+
+        ApplyVelocity(m_Velocity);
+
+        Vector3 playerCenter = transform.position;
+
+
+        playerCenter.y = MathUtilities.LerpTo(DecelerationSpeed, playerCenter.y, m_CenterHeight, Time.deltaTime);
+
+        transform.position = playerCenter;
+
+
+    }
+
+
+    void UpdateStopping(float stopEaseSpeed)
+    {
+        m_Velocity = MathUtilities.LerpTo(stopEaseSpeed, m_Velocity, GroundVelocity, Time.fixedDeltaTime);
+    }
+
+    void ApplyVelocity(Vector3 velocity)
+    {
+        Vector3 velocityDiff = velocity - m_RigidBody.velocity;
+        m_RigidBody.AddForce(velocityDiff, ForceMode.VelocityChange);
+    }
+
+
+
+
+    Vector3 CalculateMoveAccel(Vector3 localMoveDir)
+    {
+        Vector3 moveAccel = localMoveDir;
+        moveAccel = transform.TransformDirection(moveAccel);
+        return moveAccel;
+    }
+
+
+
+
+
+
+
+
+    public Vector3 GroundVelocity { get; private set; }
+    public Vector3 GroundAngularVelocity { get; private set; }
+    public Vector3 GroundNormal { get; private set; }
+
+
+
+
+
+    Rigidbody m_RigidBody;
+    Vector3 m_Velocity;
+    float m_CenterHeight;
+    int m_GroundCheckMask;
+
+
+
+
+    bool m_IsCrouching; // Temp
+
+
 }
